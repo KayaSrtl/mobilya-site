b5dbb39859016f6aedc52d774d4c31d2b3ae8e7a
KayaSrtl <50380412+KayaSrtl@users.noreply.github.com>
2023-02-16 02:20:35 +0300

    Update myScript.js

--- files/myScript.js
+++ files/myScript.js
@@ -1,1 +1,80 @@
+var window_height, window_width;
 
+
+
+
+$( document ).ready(function() {
+	
+	/*$( ".left_right_buttons_swipper" ).hover(function() {
+		$(this).css({"-webkit-transform": "translateY(-5px)"});
+		$(this).addClass("swiper_button_hover");
+	}, function() {
+		$(this).css({"-webkit-transform": "translateY(0px)"});
+		$(this).removeClass("swiper_button_hover");
+	});
+	*/
+	
+	beReadyPage();
+	
+	var mySwiper = new Swiper('.swiper-container', {
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+			},
+		loop: true,
+	});
+
+	// Define the function to go to the last slide from the first slide
+	function goToLastSlide() {
+		mySwiper.slideTo(mySwiper.slides.length - 1);
+	}
+
+	// Define the function to go to the first slide from the last slide
+	function goToFirstSlide() {
+		mySwiper.slideTo(0);
+	}
+
+// Add a click event listener to the first slide to go to the last slide
+	var firstSlide = document.querySelector('.swiper-slide:first-of-type');
+	firstSlide.addEventListener('click', function() {
+		if (mySwiper.activeIndex === 0) {
+			goToLastSlide();
+		}
+	});
+
+	// Add a click event listener to the last slide to go to the first slide
+	var lastSlide = document.querySelector('.swiper-slide:last-of-type');
+	lastSlide.addEventListener('click', function() {
+		if (mySwiper.activeIndex === mySwiper.slides.length - 1) {
+			goToFirstSlide();
+		}
+	});
+});
+
+
+
+$(window).scroll(function(event){
+	
+	
+});
+
+
+
+
+function beReadyPage() {
+	window_height = parseInt($( window ).height());
+	window_width = parseInt($( window ).width());
+	$(".swiper-container-wrapper").css("height", window_height);
+}
+
+setTimeout(function() { changeImgg(); }, 5000);
+
+function changeImgg() {
+	if(is_change_on_going && !is_trans_button_clicked)
+		transImg(transNum+1);
+	is_trans_button_clicked = false;
+	setTimeout(function() { changeImgg(); }, 5000);
+}
+
+setTimeout(function() { beReadyPage();}, 200);
+setTimeout(function() { beReadyPage();}, 500);
