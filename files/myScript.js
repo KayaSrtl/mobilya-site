eccc9a010d3b400d3e822ce3240b377024947e84
KayaSrtl <50380412+KayaSrtl@users.noreply.github.com>
2023-02-16 18:37:15 +0300

    Update myScript.js

--- files/myScript.js
+++ files/myScript.js
@@ -13,6 +13,14 @@ $( document ).ready(function() {
 	});
 	*/
 	
+	$(".icon.whatsapp").on('click', function(){
+		window.open('https://wa.me/xxxxxxxxxxxxx', '_blank');
+	});
+	
+	$(".icon.map.TURKEY").on('click', function(){
+		window.open('https://www.google.com/maps', '_blank');
+	});
+	
 	beReadyPage();
 	
 	var mySwiper = new Swiper('.swiper-container', {
@@ -58,12 +66,30 @@ $(window).scroll(function(event){
 });
 
 
+$( window ).resize(function() {
+	beReadyPage();
+	setTimeout(function() { beReadyPage();}, 100);
+	return;
+});
+
 
 
 function beReadyPage() {
 	window_height = parseInt($( window ).height());
 	window_width = parseInt($( window ).width());
 	$(".swiper-container-wrapper").css("height", window_height);
+	
+	if(window_width < 620) { 
+		$(".mapouter").css("width", window_width - 20);
+		$(".gmap_iframe").css("width", window_width - 20);
+		$(".gmap_canvas").css("width", window_width - 20);
+		document.getElementById('map1').style.width = ((window_width - 20) + "px");
+	} else {
+		$(".mapouter").css("width", 600);
+		$(".gmap_iframe").css("width", 600);
+		$(".gmap_canvas").css("width", 600);
+		document.getElementById('map1').style.width = '600px';
+	}
 }
 
 /*setTimeout(function() { changeImgg(); }, 5000);
